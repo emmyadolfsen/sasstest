16fb0d288b820b2e45226a25b57300ee26e325e1
Emmy Adolfsen <emmy.adolfsen@gmail.com>
2020-09-10 14:14:10 +0200

    trying to remove src from repo

--- src/js/fadein.js
+++
@@ -1,8 +0,0 @@
-// Fade in links when loading page
-$(document).ready(function() {
-    $("#link-1").fadeIn(2000);
-    $("#link-2").fadeIn(2500);
-    $("#link-3").fadeIn(3000);
-    $("#link-4").fadeIn(3500);
-    $("#link-5").fadeIn(4000);
-});
