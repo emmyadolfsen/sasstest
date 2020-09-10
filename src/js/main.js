6b337449badc3e09dabd00730949fed0c1c9b837
Emmy Adolfsen <emmy.adolfsen@gmail.com>
2020-09-10 21:48:08 +0200

    minor changes in style

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     // jQuery methods go here...
     function sassText() {
         var text = ("Den här sidan Gulpar med SASS. Och nu har jag fått dig att stanna kvar tills texten är slut!");
-        var delay = 200;
+        var delay = 100;
         var elem = $("#one-by-one");
         //text- string
         //elem - jQuery element where text is to be attached
