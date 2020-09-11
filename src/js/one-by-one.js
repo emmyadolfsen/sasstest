16f42f4ec26a1305c0c9c2b895859241a4569d52
Emmy Adolfsen <emmy.adolfsen@gmail.com>
2020-09-11 08:17:33 +0200

    added comments in js

--- src/js/one-by-one.js
+++ src/js/one-by-one.js
@@ -1,14 +1,12 @@
 $(document).ready(function() {
-    //setInterval(sassText, 1000);
+
     sassText();
-    // jQuery methods go here...
+
     function sassText() {
         var text = ("Den här sidan Gulpar med SASS. Och nu har jag fått dig att stanna kvar tills texten är slut!");
         var delay = 100;
         var elem = $("#one-by-one");
-        //text- string
-        //elem - jQuery element where text is to be attached
-        //delay - the delay in each text
+
         var addTextByDelay = function(text, elem, delay) {
             if (!elem) {
                 elem = $("body");
